bd8d9f96ef9100a05cb270a70fc7dd8ff22410e2
Slava <shukov.sl@gmail.com>
2022-09-29 13:31:03 +0300

    dsdsads

--- koodi.js
+++ koodi.js
@@ -1,3 +1,15 @@
-function testi(){
-    
+function IDtesti(arvo){
+    if (arvo.length < 6){
+        document.getElementById("virhe").style.display = "inline";
+    } else {
+        document.getElementById("virhe").style.display = "none";
+    }
+}
+
+function tyhja(value, id){
+    if (value.length < 1){
+        id.style.display = "inline";
+    } else {
+        id.style.display = "none";
+    }
 }
